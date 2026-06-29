e6cff4d2c642f241239b4148839030ea5edc2c7c
Nashhhh17 <naalvyy@student.ub.ac.id>
2026-06-29 13:36:57 +0700

    tailwind fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,15 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dusk-blue": "#3d5a80",
+        "powder-blue": "#98c1d9",
+        "light-cyan": "#e0fbfc",
+        "burnt-peach": "#ee6c4d",
+        "jet-black": "#293241",
+      },
+    },
   },
   plugins: [],
 };
